90e2614e7a88e72715d334dee96aeaa693a8ee23
Hamza75950 <hamza.niazi004@gmail.com>
2024-08-21 17:58:49 +0500

    created auth end point and add validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.get("/",(req,res)=>{
-    obj = {
-        a:"Hamza",
-        number: 34
+//Create a User using POST: "/api/auth/" dosent require auth.
+
+router.post(
+  "/",
+  [
+    body(
+      "name",
+      "Enter a valid name and it should be minimum 3 character long "
+    ).isLength({ min: 3 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "password should be minimum 8 character long ").isLength({
+      min: 8,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj)
 
-})
+    // Proceed with signup if no validation errors
+    User.create({
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({ error: "Email is already been use" ,message: err.message});
+      });
+  }
+);
 
-module.exports = router
+module.exports = router;
